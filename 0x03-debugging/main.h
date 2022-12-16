f54ef4578513cf1a9f480836677cd88cf1285adf
ykaylad <yinklad2k@gmail.com>
2022-12-16 13:58:28 -0800

    Task3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+void		(int i);
+int	(int a, int b, int c);
+void		(int month, int day, int year);
+int	(int month, int day);
 #endif
