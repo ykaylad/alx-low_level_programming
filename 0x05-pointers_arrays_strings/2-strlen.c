0e2bc098d37b79ff7dc2e09af6086de1338d9228
ykaylad <yinklad2k@gmail.com>
2022-12-19 05:24:29 -0800

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,9 +9,11 @@
  */
 int _strlen(char *s)
 {
-	int index;
+	int index = 0;
 
-	for (index = 0; s[index] != '\0' ; index++)
-
-		return (index);
+	while (*s != '\0')
+	{index++;
+		S++;
+	}
+	return (index);
 }
