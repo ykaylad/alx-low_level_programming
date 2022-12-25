d6fd40e265494b48fc6fdfba3e508638a319aa98
ykaylad <yinklad2k@gmail.com>
2022-12-25 13:58:35 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  * @s1: first string
  * @s2: second string
  * Return:
- * 		returns zero if s1 == s2
- * 		returns negative number if s1 < s2
- * 		returns positive number if s1 > s2
+ *		returns zero if s1 == s2
+ *		returns negative number if s1 < s2
+ *		returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		else if (s1[i] == '\0)
+		else if (s1[i] == '\0')
 		{
 			diff = s2[i];
 			break;
