c7d3f71e67272bfbe0763c728ea076a07c2dc292
ykaylad <yinklad2k@gmail.com>
2022-12-28 15:00:01 -0800

    task3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ void reset_to_98(int *n);
 	void swap_int(int *a, int *b);
 	int _strlen(char *s);
 	void _puts(char *str);
-	int _putchar(char *c);
+	int _putchar(char *);
 	void print_rev(char *s);
 	void puts2(char *str);
 	void puts_half(char *str);
