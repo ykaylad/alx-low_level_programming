c7419ebcf3b8bdc3afb771cee72e80fafaa1d64e
ykaylad <yinklad2k@gmail.com>
2022-12-29 13:36:04 -0800

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - print num from 0-9
- * Return: void
+ * Return: Always 0.
  */
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 48; c < 58; c++)
 	{
 		_putchar(c);
 	}
